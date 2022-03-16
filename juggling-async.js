1d6728ca1e17a3c52e823609b1bd1e31206a6197
RADHIARH <radhia.rahmani@sesame.com.tn>
2022-03-16 18:09:31 +0100

    send to git hub

--- juggling-async.js
+++ juggling-async.js
@@ -1,18 +1,17 @@
-const http=require("http");
-const process=require("process")
-const url1=process.argv[2]
-const url2=process.argv[3]
-const url3=process.argv[4]
-const url=[url1,url2,url3]
-url.forEach((url,)=>{
-    http.get(url, function (response) {
-      let str = "";
-      response.on("data", (chunk) => {
-        str += chunk;
-      });
-      response.on("end", () => {
-        console.log(str);
-      });
+const http = require("http");
+const process = require("process");
+const url1 = process.argv[2];
+const url2 = process.argv[3];
+const url3 = process.argv[4];
+const url = [url1, url2, url3];
+url.map((url) => {
+  http.get(url, (response) => {
+    let str = "";
+    response.on("data", (chunk) => {
+      str += chunk;
     });
-})
-
+    response.on("end", () => {
+      console.log(str);
+    });
+  });
+});
--- time-server.js
+++ time-server.js
@@ -1,14 +1,17 @@
-const net=require("net");
-const process=require("process")
-const port=process.argv[2]
-const server=net.createServer(function(socket){
-    const date=new Date();
-    const year=date.getFullYear();
+const net = require("net");
+const process = require("process");
+const port = process.argv[2];
+const server = net
+  .createServer(function (socket) {
+    const date = new Date();
+    const year = date.getFullYear();
     const Month = ("0" + (date.getMonth() + 1)).slice(-2);
     const day = ("0" + date.getDate()).slice(-2);
     const Hour = date.getHours();
-    const minute = date.getMinutes();
-    const data=year+"-"+Month+"-"+day+" "+Hour+":"+minute+'\n';
+    const minute = ("0"+date.getMinutes()).slice(-2);
+    const data =
+      year + "-" + Month + "-" + day + " " + Hour + ":" + minute + "\n";
     socket.write(data);
-    socket.end();})
-    .listen(Number(port))
+    socket.end();
+  })
+  .listen(Number(port));
